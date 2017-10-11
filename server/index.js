365314318a97d29c864b328e7aee81f8ca7d4878
Thomas Marc Beetz <siteraffer@gmail.com>
2017-10-11 09:56:40 +0200

    WIP google authentication

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
 
 const app = express();
+
+passport.use(new GoogleStrategy());
+
+
+
+// starting with oAuth authentication
+app.post(
+  '/login',
+  passport.authenticate(
+    'local',
+    { successRedirect: '/',
+      failureRedirect: '/login'
+    }
+  ),
+  function returnUser(req, res) {
+    res.redirect('/user' + req.user.username);
+  }
+);
 
 app.get('/', (req, res) => {
   res.send({fucking: 'shit'});
